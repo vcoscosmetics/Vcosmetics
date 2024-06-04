5e22b45bdbdb17e0b834343955bd4cbd44639de2
vcoscosmetics <vcoscosmetics@gmail.com>
2024-06-04 13:17:15 +0530

    save save

--- index.php
+++ index.php
@@ -393,9 +393,7 @@
 				<div class="col-lg-4 col-md-6">
 					<div class="service-single-box wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="1s">
 						<div class="service-thumb">
-							<img style="height:250px"
-								src="assets/images/service/faceCare.jpg"
-								alt="">
+							<img style="height:250px" src="assets/images/service/faceCare.jpg" alt="">
 							<div class="service-icon">
 								<img src="assets/images/service/service-icon1.png" alt="">
 							</div>
@@ -435,9 +433,7 @@
 				<div class="col-lg-4 col-md-6">
 					<div class="service-single-box wow fadeInUp" data-wow-delay="0.8s" data-wow-duration="1s">
 						<div class="service-thumb">
-							<img style="height:250px"
-								src="assets/images/service/hairCare.jpg"
-								alt="">
+							<img style="height:250px" src="assets/images/service/hairCare.jpg" alt="">
 							<div class="service-icon">
 								<img src="assets/images/service/service-icon3.png" alt="">
 							</div>
@@ -984,73 +980,11 @@
 	<!--==================================================-->
 	<!-- Start Footer Area -->
 	<!--==================================================-->
-	<div class="footer-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-3 col-md-6">
-					<div class="footer-wiget wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="1s">
-						<div class="footer-wiget-title">
-							<h4>Quick link</h4>
-						</div>
-						<div class="footer-wiget-menu">
-							<ul>
-								<li><a href="#">About Us</a></li>
-								<li><a href="#">Our Products</a></li>
-								<li><a href="#">Our Vision</a></li>
-								<li><a href="#">Latest News</a></li>
-								<li><a href="#">Contact Us</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6">
-					<div class="footer-wiget inner2 wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="1s">
-						<div class="footer-wiget-title">
-							<h4>Our Service</h4>
-						</div>
-						<div class="footer-wiget-menu">
-							<ul>
-								<li><a href="#">Third-party Manufacturing</a></li>
-								
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6">
-					<div class="footer-wiget inner">
-						<div class="footer-wiget-logo">
-							<a href="index.html"><img src="assets/images/footer-logo.png" alt=""></a>
-						</div>
-						<div class="footer-wiget-text">
-							<h4>Vcos Cosmetics has been dedicated to harnessing nature's power with our premium cosmetic products.</h4>
-						</div>
-						<div class="footer-social">
-							<ul>
-								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
-								<li><a href="#"><i class="fab fa-instagram"></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 pr-0">
-					<div class="footer-wiget inner3 wow fadeInUp" data-wow-delay="0.8s" data-wow-duration="1s">
-						<div class="footer-wiget-title">
-							<h4>Working Hours</h4>
-						</div>
-						<div class="company-work-hour">
-							<ul>
-								<li>Mon - Wed <span class="table-text">10.00 AM - 6.00 PM</span></li>
-								<li>Thu - Fri <span>10.00 AM - 6.00 PM</span></li>
-								<li>Saturday <span>10.00 AM - 6.00 PM</span></li>
-								<li class="table-brb">Sunday <span class="style">Closed</span></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php
+
+	include ("footer.php");
+
+	?>
 	<!--==================================================-->
 	<!-- End Footer Area -->
 	<!--==================================================-->
