86d9427942dff9d9fdccb1eba5c681328bb25fdf
vcoscosmetics <vcoscosmetics@gmail.com>
2024-06-04 11:19:29 +0530

    save

--- about.php
+++ about.php
@@ -54,32 +54,7 @@
 	<!--==================================================-->
 	<!-- Start Topbar Area -->
 	<!--==================================================-->
-	<div class="topbar-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-9">
-					<div class="topbar-items">
-						<ul>
-							<li class="line"><i class="bi bi-geo-alt"></i> <a href="#">103 / B New Market Broklyan -
-									stee</a></li>
-							<li><i class="bi bi-alarm-fill"></i> <a href="#">Mon - Fri: 9:00 - 19:00/ Closed </a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-3">
-					<div class="topbar-social">
-						<h4>Follow on:</h4>
-						<ul>
-							<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
-							<li><a href="#"><i class="fab fa-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+
 	<?php
 
 include ("topbar.php");
--- index.php
+++ index.php
@@ -53,32 +53,11 @@
 	<!--==================================================-->
 	<!-- Start Topbar Area -->
 	<!--==================================================-->
-	<div class="topbar-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-9">
-					<div class="topbar-items">
-						<ul>
-							<li class="line"><i class="bi bi-geo-alt"></i> <a href="#">Plot no. 12, New DLF,
-									Faridabad,Hr</a></li>
-							<li><i class="bi bi-alarm-fill"></i> <a href="#">Mon - Sat: 9:00 - 18:00/ Closed </a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-3">
-					<div class="topbar-social">
-						<h4>Follow on:</h4>
-						<ul>
-							<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
-							<li><a href="#"><i class="fab fa-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php
+
+include ("topbar.php");
+
+?>
 	<!--==================================================-->
 	<!-- End Topbar Area -->
 	<!--==================================================-->
