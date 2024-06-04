c062723b31a5817c6b68a30d78887edc60d6546c
vcoscosmetics <vcoscosmetics@gmail.com>
2024-06-04 12:01:44 +0530

    save

--- index.php
+++ index.php
@@ -62,49 +62,6 @@
 	<!-- End Topbar Area -->
 	<!--==================================================-->
 
-
-	<!--==================================================-->
-	<!-- Start Header Area -->
-	<!--==================================================-->
-	<!--<header class="header-area" id="sticky-header">-->
-	<!--	<div class="container">-->
-	<!--		<div class="row align-items-center">-->
-	<!--			<div class="col-lg-2">-->
-	<!--				<div class="logo">-->
-	<!--					<a href="index.html"><img src="assets/images/logo.png" alt=""></a>-->
-	<!--				</div>-->
-	<!--			</div>-->
-	<!--			<div class="col-lg-10">-->
-	<!--				<div class="header-menu">-->
-	<!--					<ul>-->
-	<!--						<li><a href="#">Home</a>-->
-	<!--						</li>-->
-	<!--						<li><a href="about.html">About Us</a></li>-->
-	<!--						<li><a href="#">Products </a>-->
-	<!--						</li>-->
-	<!--						<li><a href="service.html">Service </a>-->
-	<!--						</li>-->
-	<!--						<li><a href="#">FAQ's </a>-->
-	<!--						</li>-->
-	<!--						<li><a href="contact.html">Contacts Us</a></li>-->
-	<!--					</ul>-->
-	<!--					<div class="header-right">-->
-	<!--						<div class="header-search">-->
-	<!--							<a class="search-box-btn search-box-outer" href="#"><i class="bi bi-search"></i></a>-->
-	<!--						</div>-->
-	<!--						<div class="Lifesafe-btn">-->
-	<!--							<a href="#">Call Now</a>-->
-	<!--						</div>-->
-	<!--						<div class="header-sidebar">-->
-	<!--							<a class="navSidebar-button" href="#"><i class="bi bi-grid-3x3-gap-fill"></i></a>-->
-	<!--						</div>-->
-	<!--					</div>-->
-	<!--				</div>-->
-	<!--			</div>-->
-	<!--		</div>-->
-	<!--	</div>-->
-	<!--</header>-->
-
 	<?php
 
 	include ("header.php");
@@ -1038,8 +995,8 @@
 						<div class="footer-wiget-menu">
 							<ul>
 								<li><a href="#">About Us</a></li>
-								<li><a href="#">Testimonials</a></li>
-								<li><a href="#">Our Mission</a></li>
+								<li><a href="#">Our Products</a></li>
+								<li><a href="#">Our Vision</a></li>
 								<li><a href="#">Latest News</a></li>
 								<li><a href="#">Contact Us</a></li>
 							</ul>
@@ -1053,11 +1010,8 @@
 						</div>
 						<div class="footer-wiget-menu">
 							<ul>
-								<li><a href="#">Travel Insurance</a></li>
-								<li><a href="#">Life Insurance</a></li>
-								<li><a href="#">House Insurance</a></li>
-								<li><a href="#">Car Insurance</a></li>
-								<li><a href="#">Family Insurance</a></li>
+								<li><a href="#">Third-party Manufacturing</a></li>
+								
 							</ul>
 						</div>
 					</div>
