f5052c01c282a9bd3f61ddaad576d6acf8388218
vcoscosmetics <vcoscosmetics@gmail.com>
2024-06-04 15:17:27 +0530

    save

--- service.php
+++ service.php
@@ -123,10 +123,10 @@
 				<div class="col-lg-12">
 					<div class="section-title text-center wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="1s">
 						<div class="section-sub-title">
-							<h4>Our Best Services</h4>
+							<h4>Our Services</h4>
 						</div>
 						<div class="section-main-title">
-							<h2>Amazing Insurance Services</h2>
+							<h2>Third Party Manufacturing</h2>
 						</div>
 					</div>
 				</div>
@@ -135,15 +135,15 @@
 				<div class="col-lg-4 col-md-6">
 					<div class="service-single-box wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="1s">
 						<div class="service-thumb">
-							<img src="assets/images/service/service1.png" alt="">
+							<img style="height:250px" src="assets/images/service/faceCare.jpg" alt="">
 							<div class="service-icon">
 								<img src="assets/images/service/service-icon1.png" alt="">
 							</div>
 						</div>
 						<div class="service-content">
-							<h4><a href="service-details.html"> Family Insurance</a></h4>
-							<p>The trust then manages the policy 
-								and pays the premiums.</p>
+							<h4><a href="service-details.html"> Face Care</a></h4>
+							<p>We manage the formulation and ensure premium quality in every product.</p>
+
 						</div>
 						<div class="Lifesafe-btn service">
 							<a href="#">Read More +</a>
@@ -153,15 +153,15 @@
 				<div class="col-lg-4 col-md-6">
 					<div class="service-single-box wow fadeInUp" data-wow-delay="0.6s" data-wow-duration="1s">
 						<div class="service-thumb">
-							<img src="assets/images/service/service2.png" alt="">
+							<img style="height:250px" src="assets/images/service/bodyCare.jpeg" alt="">
 							<div class="service-icon">
 								<img src="assets/images/service/service-icon2.png" alt="">
 							</div>
 						</div>
 						<div class="service-content">
-							<h4><a href="service-details.html"> Business Insurance</a></h4>
-							<p>The trust then manages the policy 
-								and pays the premiums.</p>
+							<h4><a href="service-details.html"> Body Care</a></h4>
+							<p>Our team oversees formulation and covers all costs to deliver premium products to you.
+
 						</div>
 						<div class="Lifesafe-btn service">
 							<a href="#">Read More +</a>
@@ -171,15 +171,15 @@
 				<div class="col-lg-4 col-md-6">
 					<div class="service-single-box wow fadeInUp" data-wow-delay="0.8s" data-wow-duration="1s">
 						<div class="service-thumb">
-							<img src="assets/images/service/service3.png" alt="">
+							<img style="height:250px" src="assets/images/service/hairCare.jpg" alt="">
 							<div class="service-icon">
 								<img src="assets/images/service/service-icon3.png" alt="">
 							</div>
 						</div>
 						<div class="service-content">
-							<h4><a href="service-details.html"> Trusted Company</a></h4>
-							<p>The trust then manages the policy 
-								and pays the premiums.</p>
+							<h4><a href="service-details.html"> Hair Care</a></h4>
+							<p>We handle the formulation process to deliver top-quality products.
+							</p>
 						</div>
 						<div class="Lifesafe-btn service">
 							<a href="#">Read More +</a>
