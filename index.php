2eb7aa74aad8e136f87c36522648369506b33c71
vcoscosmetics <vcoscosmetics@gmail.com>
2024-06-08 12:33:08 +0530

    sss

--- index.php
+++ index.php
@@ -521,7 +521,7 @@
 				<div class="col-lg-6 col-md-12">
 					<div class="choose-thumb-items">
 						<div class="choose-thumb wow fadeInLeft" data-wow-delay="0.4s" data-wow-duration="1s">
-							<img src="assets/images/resource/choose1.jpg" style="height:250px; width: 375px;" alt="">
+							<img src="assets/images/resource/choosepic1.jpg" style="height:250px; width: 375px;" alt="">
 						</div>
 						<div class="choose-thumb wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="1s">
 							<img src="assets/images/resource/choose2.jpg" style="height:257px; width:373px;" alt="">
