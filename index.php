2f19d59aebdbddc89954dbd653ac6797a26be8c9
vcoscosmetics <vcoscosmetics@gmail.com>
2024-06-08 14:56:02 +0530

    sane

--- index.php
+++ index.php
@@ -521,7 +521,7 @@
 				<div class="col-lg-6 col-md-12">
 					<div class="choose-thumb-items">
 						<div class="choose-thumb wow fadeInLeft" data-wow-delay="0.4s" data-wow-duration="1s">
-							<img src="assets/images/resource/choosepic2.jpg" style="height:371px; width: 373px;" alt="">
+							<img src="assets/images/resource/choosepic2.jpg" style="height:300px; width: 373px;" alt="">
 						</div>
 						<div class="choose-thumb wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="1s">
 							<img src="assets/images/resource/choose2.jpg" style="height:257px; width:373px;" alt="">
