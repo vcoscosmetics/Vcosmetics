7671c271f3e8944b35501be238a52e6b5a49e6ef
sunny01 <sunnyvats1201@gmail.com>
2024-06-15 15:00:00 +0530

    sqva

--- error.php
+++ error.php
@@ -144,89 +144,11 @@
 	<!--==================================================-->
 	<!-- Start Sidebar Area -->
 	<!--==================================================-->
-	<div class="sidebar-group info-group">
-		<div class="sidebar-widget">
-			<div class="sidebar-widget-container">
-				<div class="widget-heading">
-					<a href="#" class="close-side-widget">
-						<i class="bi bi-x-lg"></i>
-					</a>
-				</div>
-				<div class="sidebar-textwidget">
-					<div class="sidebar-info-contents">
-						<div class="content-inner">
-							<div class="sidebar-logo">
-								<a href="index.html"><img src="assets/images/logo.png" alt="logo"></a>
-							</div>
-							<div class="sidebar-widget-menu">
-								<ul>
-									<li class="dropdown"><a href="#" data-toggle="dropdown">Home <i
-												class="icon-arrow"></i></a>
-										<ul class="dropdown-menu">
-											<li><a href="index.html">Home One</a></li>
-											<li><a href="index-2.html">Home Two</a></li>
-											<li><a href="index-3.html">Home Three</a></li>
-										</ul>
-									</li>
-									<li class="dropdown"><a href="about.html" data-toggle="dropdown">About</a></li>
-									<li class="dropdown"><a href="#" data-toggle="dropdown">Service <i
-												class="icon-arrow"></i></a>
-										<ul class="dropdown-menu">
-											<li><a href="service.html">Services</a></li>
-											<li><a href="life-insurance.html">Life Insurance</a></li>
-											<li><a href="car-insurance.html">Car Insurance</a></li>
-											<li><a href="health-insurance.html">Health Insurance</a></li>
-											<li><a href="home-insurance.html">Home Insurance</a></li>
-											<li><a href="travel-insurance.html">Travel Insurance</a></li>
-											<li><a href="marrige-insurance.html">Marrige Insurance</a></li>
-										</ul>
-									</li>
-									<li class="dropdown"><a href="#project" data-toggle="dropdown">Pages <i
-												class="icon-arrow"></i></a>
-										<ul class="dropdown-menu">
-											<li><a href="service.html">Service</a></li>
-											<li><a href="project-grid.html">Project Grid</a></li>
-											<li><a href="projects-details.html">Project Details</a></li>
-											<li><a href="team.html">Team</a></li>
-											<li><a href="team-detials.html">Team Details</a></li>
-											<li><a href="faq.html">Faq Page</a></li>
-											<li><a href="contact.html">Contact</a></li>
-											<li><a href="error.html">Error Page</a></li>
-										</ul>
-									</li>
-									<li class="dropdown"><a href="#" data-toggle="dropdown">Blog <i
-												class="icon-arrow"></i></a>
-										<ul class="dropdown-menu">
-											<li><a href="blog.html">Blog</a></li>
-											<li><a href="blog-details.html">Blog Details</a></li>
-										</ul>
-									</li>
-									<li class="dropdown"><a href="contact.html" data-toggle="dropdown">Contacts</a></li>
-								</ul>
-							</div>
-							<div class="contact-info">
-								<h2>Contact Info</h2>
-								<ul class="list-style-one">
-									<li><i class="bi bi-geo-alt-fill"></i>6391 Elgin St. Celina, Delaware</li>
-									<li><i class="bi bi-telephone-fill"></i>(+001) 123-456-789</li>
-									<li><i class="bi bi-envelope"></i> info@example.com</li>
-									<li><i class="bi bi-alarm-fill"></i> Week Days: 08.00 to 10.00
-									</li>
-								</ul>
-							</div>
-							<ul class="social-box">
-								<li class="facebook"><a href="#" class="fab fa-facebook-f"></a></li>
-								<li class="twitter"><a href="#" class="fab fa-instagram"></a></li>
-								<li class="linkedin"><a href="#" class="fab fa-twitter"></a></li>
-								<li class="instagram"><a href="#" class="fab fa-pinterest-p"></a></li>
-								<li class="youtube"><a href="#" class="fab fa-linkedin-in"></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php
+
+include ("sidebar.php");
+
+?>
 	<!--==================================================-->
 	<!-- End Sidebar Area -->
 	<!--==================================================-->
--- topbar.php
+++ topbar.php
@@ -4,7 +4,9 @@
 				<div class="col-lg-9">
 					<div class="topbar-items">
 						<ul>
-							<li class="line"><i class="bi bi-geo-alt"></i> <a href="#">S-67, Okhla Industrial Area, Phase II, New Delhi-110020</a></li>
+							<li class="line"><i class="bi bi-geo-alt"></i> <a href="#">
+								
+							</a></li>
 							<li><i class="bi bi-alarm-fill"></i> <a href="#">Mon - Sat: 10:00 - 18:00/ Sunday: Closed </a></li>
 						</ul>
 					</div>
