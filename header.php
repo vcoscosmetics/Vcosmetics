51a06fd4847d162acbb746fa5b5952f830ac48fa
sunny01 <sunnyvats1201@gmail.com>
2024-06-15 15:01:59 +0530

    save

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 			<div class="row align-items-center">
 				<div class="col-lg-2">
 					<div class="logo">
-						<a href="index.html"><img src="assets/images/logo.png" alt=""></a>
+						<a href="index.php"><img src="assets/images/logo.png" alt=""></a>
 					</div>
 				</div>
 				<div class="col-lg-10">
