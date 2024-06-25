4eff7cc550cf3a4e57098e58cdb7f328cb277de9
sunnyvats01 <sunnyvats1201@gmail.com>
2024-06-25 16:49:49 +0530

    save

--- products.php
+++ products.php
@@ -2,217 +2,241 @@
 <html lang="en-US">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>Vcos Cosmetics</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Vcos Cosmetics</title>
 
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Favicon -->
-	<link rel="icon" type="image/png" sizes="56x56" href="assets/images/fav-icon/icon.png">
-	<!-- bootstrap CSS -->
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" media="all">
-	<!-- carousel CSS -->
-	<link rel="stylesheet" href="assets/css/owl.carousel.min.css" type="text/css" media="all">
-	<!-- animate CSS -->
-	<link rel="stylesheet" href="assets/css/animate.css" type="text/css" media="all">
-	<!-- font-awesome CSS -->
-	<link rel="stylesheet" href="assets/css/all.min.css" type="text/css" media="all">
-	<!-- font-flaticon CSS -->
-	<link rel="stylesheet" href="assets/css/flaticon.css" type="text/css" media="all">
-	<!-- theme-default CSS -->
-	<link rel="stylesheet" href="assets/css/theme-default.css" type="text/css" media="all">
-	<!-- meanmenu CSS -->
-	<link rel="stylesheet" href="assets/css/meanmenu.min.css" type="text/css" media="all">
-	<!-- transitions CSS -->
-	<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css" media="all">
-	<!-- venobox CSS -->
-	<link rel="stylesheet" href="venobox/venobox.css" type="text/css" media="all">
-	<!-- bootstrap icons -->
-	<link rel="stylesheet" href="assets/css/bootstrap-icons.css" type="text/css" media="all">
-	<!-- Slick Slider -->
-	<link rel="stylesheet" type="text/css" href="assets/slick/slick.css">
-	<link rel="stylesheet" type="text/css" href="assets/slick/slick-theme.css">
-	<!-- Main Style CSS -->
-	<link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all">
-	<!-- Dropdown CSS -->
-	<link rel="stylesheet" href="assets/css/dropdown.css" type="text/css" media="all">
-	<!-- responsive CSS -->
-	<link rel="stylesheet" href="assets/css/responsive.css" type="text/css" media="all">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="56x56" href="assets/images/fav-icon/icon.png">
+    <!-- bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" media="all">
+    <!-- carousel CSS -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css" type="text/css" media="all">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="assets/css/animate.css" type="text/css" media="all">
+    <!-- font-awesome CSS -->
+    <link rel="stylesheet" href="assets/css/all.min.css" type="text/css" media="all">
+    <!-- font-flaticon CSS -->
+    <link rel="stylesheet" href="assets/css/flaticon.css" type="text/css" media="all">
+    <!-- theme-default CSS -->
+    <link rel="stylesheet" href="assets/css/theme-default.css" type="text/css" media="all">
+    <!-- meanmenu CSS -->
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css" type="text/css" media="all">
+    <!-- transitions CSS -->
+    <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css" media="all">
+    <!-- venobox CSS -->
+    <link rel="stylesheet" href="venobox/venobox.css" type="text/css" media="all">
+    <!-- bootstrap icons -->
+    <link rel="stylesheet" href="assets/css/bootstrap-icons.css" type="text/css" media="all">
+    <!-- Slick Slider -->
+    <link rel="stylesheet" type="text/css" href="assets/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets/slick/slick-theme.css">
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all">
+    <!-- Dropdown CSS -->
+    <link rel="stylesheet" href="assets/css/dropdown.css" type="text/css" media="all">
+    <!-- responsive CSS -->
+    <link rel="stylesheet" href="assets/css/responsive.css" type="text/css" media="all">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-	<!-- modernizr js -->
-	<script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- modernizr js -->
+    <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
 </head>
 
 <body>
-	<!-- loder -->
+    <!-- loder -->
 
-	<?php
+    <?php
 
-	include ("preloader.php");
+    include("preloader.php");
 
-	?>
+    ?>
 
-	<!--==================================================-->
-	<!-- Start Topbar Area -->
-	<!--==================================================-->
-	<?php
+    <!--==================================================-->
+    <!-- Start Topbar Area -->
+    <!--==================================================-->
+    <?php
 
-	include ("topbar.php");
+    include("topbar.php");
 
-	?>
-	<!--==================================================-->
-	<!-- End Topbar Area -->
-	<!--==================================================-->
+    ?>
+    <!--==================================================-->
+    <!-- End Topbar Area -->
+    <!--==================================================-->
 
-	<?php
+    <?php
 
-	include ("header.php");
+    include("header.php");
 
-	?>
-
-<div style="display: flex; justify-content: left; margin-bottom:5rem; padding:5rem;">
-
-<div  class="card" style="width:20rem;  height: auto;">
-  <img  src="https://5.imimg.com/data5/UD/JM/TT/SELLER-1503280/premium-quality-aloevera-juice-500-ml-500x500.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Aloevera Juice</h5>
-    <p style="font-weight: 600;" class="card-text">Price: 198</p>
-
-    <p class="card-text">Soothing, hydrating gel extracted from aloe vera leaves, renowned for its healing properties and versatile benefits for skin and hair.
+    ?>
 
 
+    <div class="product-section" >
+
+        <div style="display: grid; grid-template-columns: repeat(4, 1fr); gap:2rem; margin-bottom:5rem; padding:5rem;">
+
+            <div class="card" style=" height: auto;">
+                <img src="https://5.imimg.com/data5/UD/JM/TT/SELLER-1503280/premium-quality-aloevera-juice-500-ml-500x500.jpg" class="card-img-top" style="height: 15rem;" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aloevera Juice</h5>
+                    <p style="font-weight: 600;" class="card-text">Price: 198</p>
+
+                    <p class="card-text">Soothing, hydrating gel extracted from aloe vera leaves, renowned for its healing properties and versatile benefits for skin and hair.
+                    </p>
+                    <a href="#" class="btn btn-primary">Queries</a>
+                </div>
+            </div>
 
 
+            <div class="card" style="  height: auto;">
+                <img src="https://5.imimg.com/data5/UD/JM/TT/SELLER-1503280/premium-quality-aloevera-juice-500-ml-500x500.jpg" class="card-img-top" style="height: 15rem;" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aloevera Juice</h5>
+                    <p style="font-weight: 600;" class="card-text">Price: 198</p>
 
-</p>
-    <a href="#" class="btn btn-primary">Queries</a>
-  </div>
-</div>
-</div>
+                    <p class="card-text">Soothing, hydrating gel extracted from aloe vera leaves, renowned for its healing properties and versatile benefits for skin and hair.
+                    </p>
+                    <a href="#" class="btn btn-primary">Queries</a>
+                </div>
+            </div>
 
 
-<div style="position:fixed; height: 100vh; width: 100%; background-color: blue;" class="popup">
-<form>
-  <div class="form-group row">
-    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+            <div class="card" style="  height: auto;">
+                <img src="https://5.imimg.com/data5/UD/JM/TT/SELLER-1503280/premium-quality-aloevera-juice-500-ml-500x500.jpg" class="card-img-top" style="height: 15rem;" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aloevera Juice</h5>
+                    <p style="font-weight: 600;" class="card-text">Price: 198</p>
+
+                    <p class="card-text">Soothing, hydrating gel extracted from aloe vera leaves, renowned for its healing properties and versatile benefits for skin and hair.
+                    </p>
+                    <a href="#" class="btn btn-primary">Queries</a>
+                </div>
+            </div>
+
+
+            <div class="card" style="  height: auto;">
+                <img src="https://5.imimg.com/data5/UD/JM/TT/SELLER-1503280/premium-quality-aloevera-juice-500-ml-500x500.jpg" class="card-img-top" style="height: 15rem;" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aloevera Juice</h5>
+                    <p style="font-weight: 600;" class="card-text">Price: 198</p>
+
+                    <p class="card-text">Soothing, hydrating gel extracted from aloe vera leaves, renowned for its healing properties and versatile benefits for skin and hair.
+                    </p>
+                    <a href="#" class="btn btn-primary">Queries</a>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+
+    include("sidebar.php");
+
+    ?>
+    <!--==================================================-->
+    <!-- End Sidebar Area -->
+    <!--==================================================-->
+    <!--==================================================-->
+    <!-- Start Footer Area -->
+    <!--==================================================-->
+    <?php
+
+    include("footer.php");
+
+    ?>
+    <!--==================================================-->
+    <!-- End Footer Area -->
+    <!--==================================================-->
+    <!--==================================================-->
+    <!-- Start scrollup section Area -->
+    <!--==================================================-->
+    <!-- scrollup section -->
+    <div class="prgoress_scrollup">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
     </div>
-  </div>
-</form>
-</div>
+
+    <!--==================================================-->
+    <!-- End scrollup section Area -->
+    <!--==================================================-->
 
 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
+    <!-- jquery js -->
+    <script src="assets/js/vendor/jquery-3.6.2.min.js"></script>
 
+    <script src="assets/js/popper.min.js"></script>
 
+    <!-- bootstrap js -->
+    <script src="assets/js/bootstrap.min.js"></script>
 
+    <!-- carousel js -->
+    <script src="assets/js/owl.carousel.min.js"></script>
 
+    <!-- counterup js -->
+    <script src="assets/js/jquery.counterup.min.js"></script>
 
+    <!-- waypoints js -->
+    <script src="assets/js/waypoints.min.js"></script>
 
-<?php
+    <!-- wow js -->
+    <script src="assets/js/wow.min.js"></script>
 
-include ("sidebar.php");
+    <!-- imagesloaded js -->
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
 
-?>
-<!--==================================================-->
-<!-- End Sidebar Area -->
-<!--==================================================-->
-<!--==================================================-->
-<!-- Start Footer Area -->
-<!--==================================================-->
-<?php
+    <!-- venobox js -->
+    <script src="venobox/venobox.js"></script>
 
-include ("footer.php");
+    <!--  animated-text js -->
+    <script src="assets/js/animated-text.js"></script>
 
-?>
-<!--==================================================-->
-<!-- End Footer Area -->
-<!--==================================================-->
-<!--==================================================-->
-<!-- Start scrollup section Area -->
-<!--==================================================-->
-<!-- scrollup section -->
-<div class="prgoress_scrollup">
-    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
-        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
-    </svg>
-</div>
+    <!-- venobox min js -->
+    <script src="venobox/venobox.min.js"></script>
 
-<!--==================================================-->
-<!-- End scrollup section Area -->
-<!--==================================================-->
+    <!-- isotope js -->
+    <script src="assets/js/isotope.pkgd.min.js"></script>
 
+    <!-- jquery meanmenu js -->
+    <script src="assets/js/jquery.meanmenu.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <!-- jquery scrollup js -->
+    <script src="assets/js/jquery.scrollUp.js"></script>
 
-<!-- jquery js -->
-<script src="assets/js/vendor/jquery-3.6.2.min.js"></script>
+    <!-- Slick Slider -->
+    <script src="assets/slick/slick.min.js"></script>
 
-<script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/jquery.barfiller.js"></script>
+    <!-- jquery js -->
 
-<!-- bootstrap js -->
-<script src="assets/js/bootstrap.min.js"></script>
+    <!-- ragrslider js -->
+    <script src="assets/js/rangeslider.js"></script>
 
-<!-- carousel js -->
-<script src="assets/js/owl.carousel.min.js"></script>
+    <!-- ragrslider js -->
+    <script src="assets/js/mixitup.min.js"></script>
 
-<!-- counterup js -->
-<script src="assets/js/jquery.counterup.min.js"></script>
+    <!-- theme js -->
+    <script src="assets/js/theme.js"></script>
 
-<!-- waypoints js -->
-<script src="assets/js/waypoints.min.js"></script>
-
-<!-- wow js -->
-<script src="assets/js/wow.min.js"></script>
-
-<!-- imagesloaded js -->
-<script src="assets/js/imagesloaded.pkgd.min.js"></script>
-
-<!-- venobox js -->
-<script src="venobox/venobox.js"></script>
-
-<!--  animated-text js -->
-<script src="assets/js/animated-text.js"></script>
-
-<!-- venobox min js -->
-<script src="venobox/venobox.min.js"></script>
-
-<!-- isotope js -->
-<script src="assets/js/isotope.pkgd.min.js"></script>
-
-<!-- jquery meanmenu js -->
-<script src="assets/js/jquery.meanmenu.js"></script>
-
-<!-- jquery scrollup js -->
-<script src="assets/js/jquery.scrollUp.js"></script>
-
-<!-- Slick Slider -->
-<script src="assets/slick/slick.min.js"></script>
-
-<script src="assets/js/jquery.barfiller.js"></script>
-<!-- jquery js -->
-
-<!-- ragrslider js -->
-<script src="assets/js/rangeslider.js"></script>
-
-<!-- ragrslider js -->
-<script src="assets/js/mixitup.min.js"></script>
-
-<!-- theme js -->
-<script src="assets/js/theme.js"></script>
-
-<!-- scroll js -->
-<script src="assets/js/script.js"></script>
+    <!-- scroll js -->
+    <script src="assets/js/script.js"></script>
 
 </body>
 
