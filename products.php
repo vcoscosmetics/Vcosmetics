f0e599d6d2784d1af2f8a8492ae5532c14c15702
vcoscosmetics <vcoscosmetics@gmail.com>
2024-06-29 17:25:53 -0700

    save

--- products.php
+++ products.php
@@ -95,7 +95,7 @@ include ("connection/conn.php");
             while ($row = mysqli_fetch_assoc($result)) {
                 ?>
                 <div class="card" style=" height: auto;">
-                    <img src="<?php echo $row['image']; ?>" class="card-img-top" style="height: 15rem;" alt="...">
+                    <img src="<?php echo $row['image']; ?>" class="card-img-top"  alt="...">
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $row['name']; ?></h5>
                         <p style="" class="card-text price">Price: <?php echo $row['price']; ?></p>
