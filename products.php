1e74d15a0fe3e4e85884af19aa48de2029b3c6f1
vcoscosmetics <vcoscosmetics@gmail.com>
2024-07-02 12:03:51 +0530

    save

--- products.php
+++ products.php
@@ -90,7 +90,7 @@ include ("connection/conn.php");
 
     <div class="product-section">
 
-    <div style="display: grid; grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); gap: 2rem; margin-bottom: 5rem; padding: 2rem;">
+    <div style="display: grid; grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); gap: 2rem; margin-bottom: 5rem; padding: 2rem;">
 
             <?php
             $sql_query = "SELECT * FROM `products-list`";
