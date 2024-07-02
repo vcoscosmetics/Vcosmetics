3147a58b0f82183a8a84c5908b27391743017ae0
vcoscosmetics <vcoscosmetics@gmail.com>
2024-07-02 19:17:29 +0530

    save

--- products.php
+++ products.php
@@ -108,6 +108,26 @@ include ("connection/conn.php");
 
 
 
+    <div class="breatcome-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12">
+					<div class="breatcome-content">
+						<div class="breatcome-title">
+							<h1>Products</h1>
+						</div>
+						<div class="bratcome-text">
+							<ul>
+								<li><a href="index.html">Home</a></li>
+								<li>Products</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 
 
 
